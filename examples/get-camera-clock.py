a7798c00c44244ff6c3b90b849b2ad9a9082e9a4
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2014-07-03 11:49:37 +0100

    Added example script to get camera date/time.
    This has only been tested on two types of camera. Other drivers will
    have different config items that I don't know about. Please report any
    that you find.

---
+++ examples/get-camera-clock.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+# python-gphoto2 - Python interface to libgphoto2
+# http://github.com/jim-easterbrook/python-gphoto2
+# Copyright (C) 2014  Jim Easterbrook  jim@jim-easterbrook.me.uk
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from datetime import datetime
+import logging
+import sys
+import time
+
+import gphoto2 as gp
+
+def main():
+    # use Python logging
+    logging.basicConfig(
+        format='%(levelname)s: %(name)s: %(message)s', level=logging.WARNING)
+    gp.check_result(gp.use_python_logging())
+    # open camera connection
+    camera = gp.check_result(gp.gp_camera_new())
+    context = gp.gp_context_new()
+    gp.check_result(gp.gp_camera_init(camera, context))
+    # get configuration tree
+    config = gp.check_result(gp.gp_camera_get_config(camera, context))
+    # find the date/time setting config item and get it
+    # name varies with camera driver
+    #   Canon EOS350d - 'datetime'
+    #   PTP - 'd034'
+    for name, fmt in (('datetime', '%Y-%m-%d %H:%M:%S'),
+                      ('d034',     None)):
+        now = datetime.now()
+        OK, datetime_config = gp.gp_widget_get_child_by_name(config, name)
+        if OK >= gp.GP_OK:
+            raw_value = gp.check_result(
+                gp.gp_widget_get_value_text(datetime_config))
+            if fmt:
+                camera_time = datetime.strptime(raw_value, fmt)
+            else:
+                camera_time = datetime.utcfromtimestamp(float(raw_value))
+            print('Camera clock:   ' + camera_time.isoformat(' '))
+            print('Computer clock: ' + now.isoformat(' '))
+            err = now - camera_time
+            if err.days < 0:
+                err = -err
+                lead_lag = 'ahead'
+                print('Camera clock is ahead by',)
+            else:
+                lead_lag = 'behind'
+            print('Camera clock is %s by %d days and %d seconds' % (
+                lead_lag, err.days, err.seconds))
+            break
+    else:
+        print('Unknown date/time config item')
+    # free allocated data
+    gp.check_result(gp.gp_widget_unref(config))
+    gp.check_result(gp.gp_camera_exit(camera, context))
+    gp.check_result(gp.gp_camera_unref(camera))
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())

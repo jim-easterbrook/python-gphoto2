5632fcc196ab1be797c0ab2afd27776266a359b3
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2015-04-07 10:19:00 +0100

    Added examples to get/set capture target
    This was requested in issue #10. Note that not all cameras support
    setting the capture target.

---
+++ examples/get-capture-target.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+# python-gphoto2 - Python interface to libgphoto2
+# http://github.com/jim-easterbrook/python-gphoto2
+# Copyright (C) 2014  Jim Easterbrook  jim@jim-easterbrook.me.uk
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import print_function
+
+import logging
+import sys
+
+import gphoto2 as gp
+
+def main():
+    # use Python logging
+    logging.basicConfig(
+        format='%(levelname)s: %(name)s: %(message)s', level=logging.WARNING)
+    gp.check_result(gp.use_python_logging())
+    # open camera connection
+    camera = gp.check_result(gp.gp_camera_new())
+    context = gp.gp_context_new()
+    gp.check_result(gp.gp_camera_init(camera, context))
+    # get configuration tree
+    config = gp.check_result(gp.gp_camera_get_config(camera, context))
+    # find the capture target config item
+    capture_target = gp.check_result(
+        gp.gp_widget_get_child_by_name(config, 'capturetarget'))
+    # print current setting
+    value = gp.check_result(gp.gp_widget_get_value(capture_target))
+    print('Current setting:', value)
+    # print possible settings
+    for n in range(gp.check_result(gp.gp_widget_count_choices(capture_target))):
+        choice = gp.check_result(gp.gp_widget_get_choice(capture_target, n))
+        print('Choice:', n, choice)
+    # clean up
+    gp.check_result(gp.gp_camera_exit(camera, context))
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())

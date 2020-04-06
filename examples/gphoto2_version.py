73b3c96e7d775e987a2671e3365a35e02625dba8
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2020-04-06 08:23:30 +0100

    Add simple example to print version info

---
+++ examples/gphoto2_version.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# python-gphoto2 - Python interface to libgphoto2
+# http://github.com/jim-easterbrook/python-gphoto2
+# Copyright (C) 2020  Jim Easterbrook  jim@jim-easterbrook.me.uk
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import print_function
+
+import sys
+
+import gphoto2 as gp
+
+
+def main():
+    print('python:', sys.version)
+    print('libgphoto2:', gp.gp_library_version(gp.GP_VERSION_VERBOSE))
+    print('python-gphoto2:', gp.__version__)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())

5805dacadcd9c4ad7271c92055ae1673c07c4f9f
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2021-08-05 14:45:17 +0100

    Fix tag_release.py imports

--- developer/tag_release.py
+++ developer/tag_release.py
@@ -15,8 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from datetime import date
-import re
+import os
 import sys
 
 # requires GitPython - 'sudo pip install gitpython'

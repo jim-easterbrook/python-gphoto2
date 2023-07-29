6c66d1ef457ebe733446c078a5d9deb7e020c465
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2023-07-29 12:36:22 +0100

    Added "C style" camera tests

--- tests/test_camera.py
+++ tests/test_camera.py
@@ -28,18 +28,33 @@ class TestNoCamera(unittest.TestCase):
         # switch from virtual camera to normal drivers
         os.environ['IOLIBS'] = os.environ['IOLIBS'].replace('vusb', 'iolibs')
 
-    def test_autodetect_and_init(self):
+    def test_oo_style(self):
         cameras = gp.Camera.autodetect()
         self.assertIsInstance(cameras, gp.CameraList)
         camera = gp.Camera()
         if len(cameras) > 0:
             # there is actually a camera connected
             camera.init()
+            camera.exit()
         else:
             with self.assertRaises(gp.GPhoto2Error) as cm:
                 camera.init()
             ex = cm.exception
-            self.assertEqual(ex.code, -105)
+            self.assertEqual(ex.code, gp.GP_ERROR_MODEL_NOT_FOUND)
+
+    def test_c_style(self):
+        OK, cameras = gp.gp_camera_autodetect()
+        self.assertGreaterEqual(OK, gp.GP_OK)
+        self.assertIsInstance(cameras, gp.CameraList)
+        OK, camera = gp.gp_camera_new()
+        self.assertEqual(OK, gp.GP_OK)
+        OK = gp.gp_camera_init(camera)
+        if len(cameras) > 0:
+            # there is actually a camera connected
+            self.assertEqual(OK, gp.GP_OK)
+            self.assertEqual(gp.gp_camera_exit(camera), gp.GP_OK)
+        else:
+            self.assertEqual(OK, gp.GP_ERROR_MODEL_NOT_FOUND)
 
 
 class TestAutoDetect(unittest.TestCase):
@@ -47,11 +62,20 @@ class TestAutoDetect(unittest.TestCase):
         # switch to virtual camera from normal drivers
         os.environ['IOLIBS'] = os.environ['IOLIBS'].replace('iolibs', 'vusb')
 
-    def test_autodetect(self):
+    def test_oo_style(self):
         cameras = gp.Camera.autodetect()
         self.assertIsInstance(cameras, gp.CameraList)
         self.assertEqual(len(cameras), 1)
         name, value = cameras[0]
+        self.assertEqual(name, 'Nikon DSC D750')
+
+    def test_c_style(self):
+        OK, cameras = gp.gp_camera_autodetect()
+        self.assertGreaterEqual(OK, gp.GP_OK)
+        self.assertIsInstance(cameras, gp.CameraList)
+        self.assertEqual(gp.gp_list_count(cameras), 1)
+        OK, name = gp.gp_list_get_name(cameras, 0)
+        self.assertEqual(OK, gp.GP_OK)
         self.assertEqual(name, 'Nikon DSC D750')
 
 
@@ -65,59 +89,125 @@ class TestVirtualCamera(unittest.TestCase):
     def tearDown(self):
         self.camera.exit()
 
-    def test_get_about(self):
+    def test_oo_style(self):
+        def list_config(widget):
+            label = '{} ({})'.format(widget.get_label(), widget.get_name())
+            result = [label]
+            widget_type = widget.get_type()
+            if widget_type in (gp.GP_WIDGET_WINDOW, gp.GP_WIDGET_SECTION):
+                for child in widget.get_children():
+                    result += list_config(child)
+            return result
+
+        def list_files(path='/'):
+            result = []
+            # get files
+            for name, value in self.camera.folder_list_files(path):
+                result.append(os.path.join(path, name))
+            # recurse over subfolders
+            for name, value in self.camera.folder_list_folders(path):
+                result.extend(list_files(os.path.join(path, name)))
+            return result
+
+        # get_about
         text = str(self.camera.get_about())
         self.assertTrue(text.startswith('PTP2 driver'))
         self.assertTrue(text.endswith('Enjoy!'))
-
-    def test_get_summary(self):
+        # get_summary
         text = str(self.camera.get_summary())
         self.assertTrue(text.startswith('Manufacturer'))
-
-    def list_config(self, widget):
-        label = '{} ({})'.format(widget.get_label(), widget.get_name())
-        result = [label]
-        widget_type = widget.get_type()
-        if widget_type in (gp.GP_WIDGET_WINDOW, gp.GP_WIDGET_SECTION):
-            for child in widget.get_children():
-                result += self.list_config(child)
-        return result
-
-    def test_get_config(self):
+        # get_config
         widget = self.camera.get_config()
-        config_list = self.list_config(widget)
+        config_list = list_config(widget)
         self.assertEqual(len(config_list), 31)
         self.assertEqual(
             config_list[0], 'Camera and Driver Configuration (main)')
-
-    def list_files(self, path='/'):
-        result = []
-        # get files
-        for name, value in self.camera.folder_list_files(path):
-            result.append(os.path.join(path, name))
-        # read folders
-        folders = []
-        for name, value in self.camera.folder_list_folders(path):
-            folders.append(name)
-        # recurse over subfolders
-        for name in folders:
-            result.extend(self.list_files(os.path.join(path, name)))
-        return result
-
-    def test_list_files(self):
-        files = self.list_files()
-        self.assertEqual(files, ['/store_00010001/copyright-free-image.jpg'])
-
-    def test_capture(self):
+        # list_files
+        files = list_files()
+        self.assertEqual(files[0], '/store_00010001/copyright-free-image.jpg')
+        # capture
         path = self.camera.capture(gp.GP_CAPTURE_IMAGE)
-        self.assertEqual(path.name, 'GPH_0099.JPG')
-        self.assertEqual(path.folder, '/store_00010001/DCIM/100GPHOT')
+        self.assertRegex(path.name, 'GPH_\d{4}.JPG')
+        self.assertRegex(path.folder, '/store_00010001/DCIM/\d{3}GPHOT')
         info = self.camera.file_get_info(path.folder, path.name).file
         self.assertEqual(info.size, 7082)
         self.assertEqual(info.type, 'image/jpeg')
         # width and height are zero as test image has no exif info
         self.assertEqual(info.width, 0)
         self.assertEqual(info.height, 0)
+        # delete file
+        self.camera.file_delete(path.folder, path.name)
+
+    def test_c_style(self):
+        def list_config(widget):
+            OK, label = gp.gp_widget_get_label(widget)
+            self.assertEqual(OK, gp.GP_OK)
+            OK, name = gp.gp_widget_get_name(widget)
+            self.assertEqual(OK, gp.GP_OK)
+            result = ['{} ({})'.format(label, name)]
+            OK, widget_type = gp.gp_widget_get_type(widget)
+            self.assertEqual(OK, gp.GP_OK)
+            if widget_type in (gp.GP_WIDGET_WINDOW, gp.GP_WIDGET_SECTION):
+                for idx in range(gp.gp_widget_count_children(widget)):
+                    OK, child = gp.gp_widget_get_child(widget, idx)
+                    self.assertEqual(OK, gp.GP_OK)
+                    result += list_config(child)
+            return result
+
+        def list_files(path='/'):
+            result = []
+            # get files
+            OK, files = gp.gp_camera_folder_list_files(self.camera, path)
+            self.assertEqual(OK, gp.GP_OK)
+            for idx in range(gp.gp_list_count(files)):
+                OK, name = gp.gp_list_get_name(files, idx)
+                self.assertEqual(OK, gp.GP_OK)
+                result.append(os.path.join(path, name))
+            # recurse over subfolders
+            OK, folders = gp.gp_camera_folder_list_folders(self.camera, path)
+            self.assertEqual(OK, gp.GP_OK)
+            for idx in range(gp.gp_list_count(folders)):
+                OK, name = gp.gp_list_get_name(folders, idx)
+                self.assertEqual(OK, gp.GP_OK)
+                result.extend(list_files(os.path.join(path, name)))
+            return result
+
+        # get_about
+        OK, text = gp.gp_camera_get_about(self.camera)
+        self.assertEqual(OK, gp.GP_OK)
+        self.assertTrue(text.text.startswith('PTP2 driver'))
+        self.assertTrue(text.text.endswith('Enjoy!'))
+        # get_summary
+        OK, text = gp.gp_camera_get_summary(self.camera)
+        self.assertEqual(OK, gp.GP_OK)
+        self.assertTrue(text.text.startswith('Manufacturer'))
+        # get_config
+        OK, widget = gp.gp_camera_get_config(self.camera)
+        self.assertEqual(OK, gp.GP_OK)
+        config_list = list_config(widget)
+        self.assertEqual(len(config_list), 31)
+        self.assertEqual(
+            config_list[0], 'Camera and Driver Configuration (main)')
+        # list_files
+        files = list_files()
+        self.assertEqual(files[0], '/store_00010001/copyright-free-image.jpg')
+        # capture
+        OK, path = gp.gp_camera_capture(self.camera, gp.GP_CAPTURE_IMAGE)
+        self.assertEqual(OK, gp.GP_OK)
+        self.assertRegex(path.name, 'GPH_\d{4}.JPG')
+        self.assertRegex(path.folder, '/store_00010001/DCIM/\d{3}GPHOT')
+        OK, info = gp.gp_camera_file_get_info(
+            self.camera, path.folder, path.name)
+        self.assertEqual(OK, gp.GP_OK)
+        info = info.file
+        self.assertEqual(info.size, 7082)
+        self.assertEqual(info.type, 'image/jpeg')
+        # width and height are zero as test image has no exif info
+        self.assertEqual(info.width, 0)
+        self.assertEqual(info.height, 0)
+        # delete file
+        self.assertEqual(gp.gp_camera_file_delete(
+            self.camera, path.folder, path.name), gp.GP_OK)
 
 
 if __name__ == "__main__":

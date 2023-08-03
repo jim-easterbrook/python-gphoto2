6a585adaa98394a1fd40a98ff59dca5b5f9f7c7a
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2023-08-03 08:51:00 +0100

    Small additions to abilities_list module tests

--- tests/test_abilities_list.py
+++ tests/test_abilities_list.py
@@ -27,13 +27,17 @@ class TestAbilitiesList(unittest.TestCase):
     def setUp(self):
         # switch to virtual camera from normal drivers
         os.environ['IOLIBS'] = os.environ['IOLIBS'].replace('iolibs', 'vusb')
+        # set locale dir (should be already set by package __init__)
+        locale_dir = os.environ['IOLIBS'].replace('vusb', 'locale')
+        self.assertEqual(gp.gp_init_localedir(locale_dir), gp.GP_OK)
+        self.assertEqual(gp.gp_message_codeset('utf-8'), 'utf-8')
 
     def test_oo_style(self):
         abilities_list = gp.CameraAbilitiesList()
         self.assertIsInstance(abilities_list, gp.CameraAbilitiesList)
         self.assertEqual(len(abilities_list), 0)
         abilities_list.load()
-        self.assertNotEqual(len(abilities_list), 0)
+        self.assertGreater(len(abilities_list), 0)
         abilities = abilities_list[0]
         self.assertIsInstance(abilities, gp.CameraAbilities)
         self.assertIsInstance(abilities.model, str)
@@ -46,6 +50,8 @@ class TestAbilitiesList(unittest.TestCase):
         self.assertIsInstance(abilities_list.lookup_model(name), int)
         abilities_list.reset()
         self.assertEqual(len(abilities_list), 0)
+        abilities_list.load_dir(os.environ['CAMLIBS'])
+        self.assertGreater(len(abilities_list), 0)
 
     def test_c_style(self):
         OK, abilities_list = gp.gp_abilities_list_new()
@@ -53,7 +59,7 @@ class TestAbilitiesList(unittest.TestCase):
         self.assertIsInstance(abilities_list, gp.CameraAbilitiesList)
         self.assertEqual(gp.gp_abilities_list_count(abilities_list), 0)
         self.assertEqual(gp.gp_abilities_list_load(abilities_list), gp.GP_OK)
-        self.assertNotEqual(gp.gp_abilities_list_count(abilities_list), 0)
+        self.assertGreater(gp.gp_abilities_list_count(abilities_list), 0)
         OK, abilities = gp.gp_abilities_list_get_abilities(abilities_list, 0)
         self.assertEqual(OK, gp.GP_OK)
         self.assertIsInstance(abilities, gp.CameraAbilities)
@@ -74,6 +80,10 @@ class TestAbilitiesList(unittest.TestCase):
         self.assertIsInstance(idx, int)
         self.assertEqual(gp.gp_abilities_list_reset(abilities_list), gp.GP_OK)
         self.assertEqual(gp.gp_abilities_list_count(abilities_list), 0)
+        self.assertEqual(
+            gp.gp_abilities_list_load_dir(abilities_list,
+                                          os.environ['CAMLIBS']), gp.GP_OK)
+        self.assertGreater(gp.gp_abilities_list_count(abilities_list), 0)
 
 
 if __name__ == "__main__":

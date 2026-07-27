b3afc1f4ed5f6e3f34d1d335cb19b2f5a73d0463
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2026-07-27 09:19:45 +0100

    Raise exception if NULL ptr in gp_widget_get_value

--- src/swig-gp2_5_34/widget_wrap.c
+++ src/swig-gp2_5_34/widget_wrap.c
@@ -4398,10 +4398,12 @@ static PyObject* from_void(CameraWidget* widget, void* value) {
     case GP_WIDGET_MENU:
     case GP_WIDGET_TEXT:
     case GP_WIDGET_RADIO:
-      if (*((char**)value))
-        return PyString_FromString(*((char**)value));
-      Py_INCREF(Py_None);
-      return Py_None;
+      if (!*((char**)value)) {
+        PyErr_SetString(PyExc_RuntimeError,
+                        "gp_widget_get_value: NULL string pointer");
+        return NULL;
+      }
+      return PyString_FromString(*((char**)value));
     default:
       PyErr_SetString(PyExc_RuntimeError, "Unsupported widget type");
   }
